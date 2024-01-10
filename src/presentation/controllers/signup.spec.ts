c189c8eb3a954a014c054a43299bf41ae9b44244
Charles Pereira <charlesti.ptu@gmail.com>
2024-01-10 20:48:45 -0300

    test: signUpController return 400 and error if no email is provider

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -16,5 +16,22 @@ describe('SignUp Controller', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  }),
+
+  it('Should return 400 if no email is provided',() => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirm: 'any_password'
+      }
+    }
+
+    const httpResponse =  sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
