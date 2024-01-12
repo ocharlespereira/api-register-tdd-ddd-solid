881f64f126baef6c086c8f0802e0af9a494639df
Charles Pereira <charlesti.ptu@gmail.com>
2024-01-11 21:33:44 -0300

    refactor: move server error to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,18 +1,14 @@
 import { AbsenceOfParamError } from '../errors/absence-of-param-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 
 import { HttpRequestProps, HttpResponseProps } from '../protocols/http'
 import { ControllerSignUp } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { InvalidParamError } from '../errors/invalid-param-error'
-import { ServerError } from '../errors/server-error'
 
 export class SignUpController implements ControllerSignUp {
-  // private readonly emailValidator: EmailValidator
 
-  constructor(private readonly emailValidator: EmailValidator) {
-    // this.emailValidator = emailValidator
-  }
+  constructor(private readonly emailValidator: EmailValidator) {}
 
   handle(httpRequest: HttpRequestProps): HttpResponseProps {
     try {
@@ -31,10 +27,7 @@ export class SignUpController implements ControllerSignUp {
         return badRequest(new InvalidParamError('email'))
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      return serverError()
     }
 
   }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,12 @@
+import { ServerError } from '../errors/server-error'
 import { HttpResponseProps } from '../protocols/http'
 
 export const badRequest = (error: Error): HttpResponseProps => ({
   statusCode: 400,
   body: error
+})
+
+export const serverError = (): HttpResponseProps => ({
+  statusCode: 500,
+  body: new ServerError()
 })
